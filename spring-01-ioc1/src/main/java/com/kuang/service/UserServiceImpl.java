c37c5ddc1cfedb4188771fe877eba7e5ba17e846
Kumiko <xiluo2233@outlook.com>
2024-09-18 21:38:17 +0800

    add requirement2

--- spring-01-ioc1/src/main/java/com/kuang/service/UserServiceImpl.java
+++ spring-01-ioc1/src/main/java/com/kuang/service/UserServiceImpl.java
@@ -3,10 +3,12 @@ package com.kuang.service;
 import com.kuang.dao.UserDao;
 import com.kuang.dao.UserDaoImpl;
 import com.kuang.dao.UserDaoMysqlImpl;
+import com.kuang.dao.UserDaoOracleImpl;
 
 public class UserServiceImpl implements UserService{
 //    private UserDao userDao = new UserServiceImpl();
-    private UserDao userDao = new UserDaoMysqlImpl();   // 更改实现, 新的用户UserDaoMysqlImpl才可以应用
+//    private UserDao userDao = new UserDaoMysqlImpl();   // 更改实现, 新的用户UserDaoMysqlImpl才可以应用
+    private UserDao userDao = new UserDaoOracleImpl();   // 更改实现, 新的用户UserDaoMysqlImpl才可以应用
 
     @Override
     public void getUser() {
